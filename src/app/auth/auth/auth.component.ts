ed32a90a27d3af5dd3b318afbeb9083036452f18
Mario Morais Neto <mariom_mmn@hotmail.com>
2024-09-23 19:51:33 -0300

    endereço e via cep no cadastro

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../services';
+import { AuthService, ViaCepResponse } from '../../services';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [NgxMaskDirective],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
+  providers: [provideNgxMask()]
 })
 export class AuthComponent {
-    private res: number = 0;
+    cep: string = '';
+    logradouro: string = '';
+    complemento: string = '';
+    unidade: string = '';
+    bairro: string = '';
+    localidade: string = '';
+    uf: string = '';
+    estado: string = '';
     
     constructor(private authService: AuthService) {}
 
-    testarBotao(numero: number): void {
-        this.res = this.authService.teste(numero);
-    }
+    onCepChange(): void {
+        if (this.cep) {
+          this.authService.getCepInfo(this.cep).subscribe((data: ViaCepResponse) => {
+            this.logradouro = data.logradouro;
+            this.complemento = data.complemento;
+            this.bairro = data.bairro;
+            this.localidade = data.localidade;
+            this.uf = data.uf;
+            this.estado = data.estado;
+          });
+        }
+      }
 
     get resultado(): string {
-        return this.res.toString();
+        return "a";
     }
+
 }
